50e214adfef734c23253d93cfe5c0ca2f5e1dcf9
Maxim Semchishin <max.semchishin@gmail.com>
2023-10-15 16:55:11 +0500

    slight rework

--- modal/modal.js
+++ modal/modal.js
@@ -1,9 +1,10 @@
 const triggerModalBtn = document.getElementById('modalButton');
 const modalBg = document.getElementById('modalBg');
 const closeButton = document.querySelector('.closeButton');
+const modalText = document.querySelector('.modal-text');
 
 triggerModalBtn.addEventListener('click', showModal)
-// closeButton.addEventListener('click', hideModal)
+
 document.addEventListener('keyup', (event) => {
     if (event.key === "Escape") {
         hideModal();
@@ -26,3 +27,22 @@ function hideModal() {
     modalBg.classList.remove('visible');
 }
 
+const getData = (url) => 
+    new Promise((resolve, reject) => 
+    fetch(url)
+    .then(response => response.json())
+    .then(json => resolve(json))
+    .catch(error => reject(error))
+    )
+
+getData('https://jsonplaceholder.typicode.com/todos/1')
+    .then(function (data) {
+        for (const key in data) {
+            const element = data[key];
+            console.log(element);
+            const li = document.createElement('li');
+            modalText.append(li);
+            li.append(key + ': ' + element);
+        }
+    })
+    .catch(error => console.log(error.message))
